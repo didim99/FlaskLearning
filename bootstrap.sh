5cba044ed8427e9ea23039aaa1b2f6c635d241ae
didim99 <didim@eclabs.ru>
2020-03-23 17:16:33 +0300

    > Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,3 +10,11 @@ python setup.py build_ext --inplace
 # create empty user table
 mkdir app/data
 echo '[]' > app/data/userdata.json
+# create default user if necessary
+DEFAULT_USER='[{"login": "root", "passw": "202cb962ac59075b964b07152d234b70"}]'
+read -p "Add default user 'root' with password '123'? (Y/N): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "$DEFAULT_USER" > app/data/userdata.json
+fi
