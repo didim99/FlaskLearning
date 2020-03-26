3671ac557a76825db211528a575dffd194be13a5
didim99 <didim@eclabs.ru>
2020-03-27 01:01:44 +0300

    ! Fixed Windows bootstrap script

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,7 +1,8 @@
 @echo off
+SETLOCAL
 REM: initialize python virtual environment
 python -m venv .\venv
-.\venv\Scripts\activate
+call .\venv\Scripts\activate
 REM: install required packages
 pip install --upgrade pip
 pip install -r requirements.txt
@@ -11,13 +12,8 @@ REM: create empty user table
 mkdir app\data
 echo [] > app\data\userdata.json
 REM: create default user if necessary
-set DEFAULT_USER=[{"login": "root", "passw": "202cb962ac59075b964b07152d234b70"}]"
-:q
-echo.
-set /p DEPT1="Add default user 'root' with password '123'? (y/n): "
-if /i not defined DEPT1 (cls & goto q)
-if /i "%DEPT1%"=="y" (echo yes& goto d)
-if /i "%DEPT1%"=="n" (echo no& goto e)
-:d
+set DEFAULT_USER=[{"login": "root", "passw": "202cb962ac59075b964b07152d234b70"}]
+set reply=n
+set /p "reply=Add default user 'root' with password '123'? (y/n): "
+if /i not "%reply%" == "y" goto :eof
 echo %DEFAULT_USER% > app\data\userdata.json
-:e
